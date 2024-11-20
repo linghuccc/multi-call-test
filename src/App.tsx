ff1bff64555c5338e1b5b57486fc0abad5fdfff0
linghuccc <li_chang_2003@hotmail.com>
2024-11-20 09:34:56 +0800

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,51 @@
 import './App.css'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import '@rainbow-me/rainbowkit/styles.css'
-import { type Address } from 'viem'
-import { contractAbi } from './contract'
+
+import { useState } from 'react'
+import { contractAbi, contractAddress } from './constants'
 import { useWriteContract, useWaitForTransactionReceipt } from 'wagmi'
 
 function App() {
-	const contractAddress: Address =
-		'0x5131b315AABc299978991531ab1a7721488FfFb0'
+	const [status, setStatus] = useState('')
 
-	const { data: action1Hash, writeContract: action1 } = useWriteContract()
+	const {
+		data,
+		error: writeError,
+		isPending,
+		writeContractAsync,
+	} = useWriteContract()
 
-	const { isSuccess: isAction1Success } = useWaitForTransactionReceipt({
-		hash: action1Hash,
+	const {
+		error: txError,
+		isLoading,
+		isSuccess,
+	} = useWaitForTransactionReceipt({
+		hash: data,
 	})
+
+	// 假设 action1 和 action2 返回 Promise
+	async function action1() {
+		try {
+			// 执行一些异步操作
+			const action1Tx = await writeContractAsync({
+				abi: contractAbi,
+				address: contractAddress,
+				functionName: 'startDelay',
+				args: [BigInt(10)],
+			})
+
+			console.log('Action 1 hash:', action1Tx)
+
+			return Promise.resolve('Action 1 completed')
+		} catch (error) {
+			console.log('Write error:', writeError)
+			console.log('Transaction error:', txError)
+			console.error('An error occurred:', error)
+
+			return Promise.resolve('Action 1 error')
+		}
+	}
 
 	function waitFor(seconds: number): Promise<void> {
 		return new Promise((resolve) => {
@@ -23,47 +55,53 @@ function App() {
 		})
 	}
 
-	const { writeContract: action2 } = useWriteContract()
-
-	const handleClick = async () => {
+	async function action2() {
 		try {
-			action1({
-				address: contractAddress,
-				abi: contractAbi,
-				functionName: 'startDelay',
-				args: [BigInt(10)],
-			})
-			console.log('isAction1Success is ', isAction1Success)
+			await waitFor(10)
 
-			if (isAction1Success) {
-				console.log('Action1 Succeeded')
-				await waitFor(10)
-				action2({
-					address: contractAddress,
-					abi: contractAbi,
-					functionName: 'doSomething',
-				})
-			}
+			// 执行一些异步操作
+			const action2Tx = await writeContractAsync({
+				abi: contractAbi,
+				address: contractAddress,
+				functionName: 'doSomething',
+			})
+
+			console.log('Action 2 hash:', action2Tx)
+
+			return Promise.resolve('Action 2 completed')
 		} catch (error) {
-			console.log('An error occurred.')
-			console.error(error)
+			console.log('Write error:', writeError)
+			console.log('Transaction error:', txError)
+			console.error('An error occurred:', error)
+
+			return Promise.resolve('Action 2 error')
+		}
+	}
+	async function handleClick() {
+		try {
+			// 等待 action1 完成
+			const res1 = await action1()
+			setStatus(res1) // 更新状态
+
+			// 等待 action2 完成
+			const res2 = await action2()
+			setStatus(res2) // 更新状态
+		} catch (error) {
+			// 处理任何可能发生的错误
+			console.error('An error occurred:', error)
 		}
 	}
 
 	return (
-		<>
-			<div className="flex items-center">
-				<ConnectButton accountStatus="address" showBalance={false} />
-			</div>
-			<div>
-				<button
-					onClick={handleClick}
-					className="text-white rounded-full mt-4 px-6 py-2 bg-gradient-to-r from-blue-500 to-indigo-600 hover:scale-105 transition-all ease-in duration-100"
-				>
-					Perform 2 actions
-				</button>
-			</div>
-		</>
+		<div className="centered">
+			<ConnectButton accountStatus="address" showBalance={false} />
+			<div className="spacer" />
+			<button disabled={isPending || isLoading} onClick={handleClick}>
+				{isPending ? 'Pending' : isLoading ? 'Waiting' : 'Run Actions'}
+			</button>
+			<div className="spacer" />
+			<p>Current status: {status}</p>
+		</div>
 	)
 }
 
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+import { contractAbi } from './contract'
+const contractAddress = '0x5131b315AABc299978991531ab1a7721488FfFb0'
+
+export { contractAbi, contractAddress }
